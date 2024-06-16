68749de21a25d84dbf59ce5b37f797103a71ac30
Samuel Kamphof <sam@eemster82.nl>
2024-06-16 11:44:51 +0200

    Make the repo gitready

---
+++ README.md
@@ -0,0 +1,5 @@
+# Make application
+write `make` in your terminal
+
+# Run application
+write `build/apps/program` in your terminal
---
+++ src/program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to the main program!" << std::endl;
+
+    return 0;
+}
